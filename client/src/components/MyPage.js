4fb002d196a5d387a69ac1ac8913071a68cf9860
test <139727181+ljs1990-tj@users.noreply.github.com>
2025-11-21 16:35:57 +0900

    2025.11.21

--- client/src/components/MyPage.js
+++ client/src/components/MyPage.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Typography, Box, Avatar, Grid, Paper } from '@mui/material';
 
 function MyPage() {
+  let [user, setUser] = useState();
+
+  function fnGetUser(){
+    // 원래 아이디를 jwt 토큰에서 꺼내야 함
+    let id = "qw123";
+    fetch("http://localhost:3010/user/" + id)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setUser(data.user);
+      })
+  }
+
+  useEffect(()=>{
+    fnGetUser();
+  }, [])
+
   return (
     <Container maxWidth="md">
       <Box
@@ -20,29 +37,29 @@ function MyPage() {
               src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e" // 프로필 이미지 경로
               sx={{ width: 100, height: 100, marginBottom: 2 }}
             />
-            <Typography variant="h5">홍길동</Typography>
+            <Typography variant="h5">{user?.userName}</Typography>
             <Typography variant="body2" color="text.secondary">
-              @honggildong
+              @{user?.userId}
             </Typography>
           </Box>
           <Grid container spacing={2} sx={{ marginTop: 2 }}>
             <Grid item xs={4} textAlign="center">
               <Typography variant="h6">팔로워</Typography>
-              <Typography variant="body1">150</Typography>
+              <Typography variant="body1">{user?.follower}</Typography>
             </Grid>
             <Grid item xs={4} textAlign="center">
               <Typography variant="h6">팔로잉</Typography>
-              <Typography variant="body1">100</Typography>
+              <Typography variant="body1">{user?.following}</Typography>
             </Grid>
             <Grid item xs={4} textAlign="center">
               <Typography variant="h6">게시물</Typography>
-              <Typography variant="body1">50</Typography>
+              <Typography variant="body1">{user?.cnt}</Typography>
             </Grid>
           </Grid>
           <Box sx={{ marginTop: 3 }}>
             <Typography variant="h6">내 소개</Typography>
             <Typography variant="body1">
-              안녕하세요! SNS를 통해 친구들과 소통하고 있습니다. 사진과 일상을 공유하는 것을 좋아해요.
+              {user?.intro}
             </Typography>
           </Box>
         </Paper>
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,6 +3,39 @@ const bcrypt = require('bcrypt');
 const router = express.Router();
 const db = require("../db");
 
+router.get("/:userId", async (req, res) => {
+    let {userId} = req.params;
+    try {
+       // 1. 두개 쿼리 써서 리턴
+    //    let [list] = await db.query("SELECT * FROM TBL_USER WHERE USERID = ?", [userId]);
+    //    let [cnt] = await db.query("SELECT COUNT(*) FROM TBL_FEED WHERE USERID = ?", [userId]);
+    //    res.json({
+    //     user : list[0],
+    //     cnt : cnt[0]
+    //    }) 
+
+       // 2. 조인쿼리 만들어서 하나로 리턴
+        let sql = "SELECT U.*, cnt "
+                + "FROM TBL_USER U "
+                + "INNER JOIN ( "
+                + "SELECT USERID, COUNT(*) CNT "
+                + "FROM TBL_FEED "
+                + "WHERE USERID = ? "
+                + ") T ON U.USERID = T.USERID";
+        let [list] = await db.query(sql, [userId]);
+        res.json({
+            user : list[0],
+            result : "success"
+        })
+
+
+
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+
 router.post("/join", async (req, res) => {
     let {userId, pwd, userName} = req.body;
     try {
